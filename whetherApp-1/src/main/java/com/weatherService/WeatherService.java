a25dcd5e5a72874c942570035a25af796f678d6e
Noorish100 <mdnoorishnauman@gmail.com>
2024-04-07 16:31:22 +0530

    addddddddddddddddd

--- whetherApp-1/src/main/java/com/weatherService/WeatherService.java
+++ whetherApp-1/src/main/java/com/weatherService/WeatherService.java
@@ -26,10 +26,11 @@ public class WeatherService {
     public String getForecastSummaryByLocationName(String cityName) {
         HttpHeaders headers = new HttpHeaders();
         headers.set("X-RapidAPI-Key", "3befd5dbd8mshc6d737112793534p10c569jsn6e335bdd937e");
-
+        headers.set("X-RapidAPI-Host", "forecast9.p.rapidapi.com");
+//        https://forecast9.p.rapidapi.com/rapidapi/forecast/Berlin/summary/
         HttpEntity<String> entity = new HttpEntity<>(headers);
         ResponseEntity<String> response = restTemplate.exchange(
-                "https://forecast9.p.rapidapi.com/RapidApiGetForecastSummaryByLocationName?cityName=" + cityName,
+                "https://forecast9.p.rapidapi.com/rapidapi/forecast/"+cityName+"/summary/",
                 HttpMethod.GET, entity, String.class);
 
         return response.getBody();
@@ -37,11 +38,11 @@ public class WeatherService {
 
     public String getHourlyForecastByLocationName(String cityName) {
         HttpHeaders headers = new HttpHeaders();
-        headers.set("X-RapidAPI-Key", "3befd5dbd8mshc6d737112793534p10c569jsn6e335bdd937e");
+//        headers.set("X-RapidAPI-Key", "3befd5dbd8mshc6d737112793534p10c569jsn6e335bdd937e");
 
         HttpEntity<String> entity = new HttpEntity<>(headers);
         ResponseEntity<String> response = restTemplate.exchange(
-                "https://forecast9.p.rapidapi.com/RapidApiGetHourlyForecastByLocationName?cityName=" + cityName,
+        		"https://api.openweathermap.org/data/2.5/forecast?q="+cityName+"&appid=429bcdfc2cd3d075f0f369a6379f8906",
                 HttpMethod.GET, entity, String.class);
 
         return response.getBody();
